e9cc895ca8c9fa62a33fb987919e788a329b4aa1
wangminglei <wangminglei@inke.cn>
2020-08-25 10:31:10 +0800

    5. 最长回文子串  动态规划 抄答案 AC 题解很优秀

---
+++ 5. 最长回文子串/Solution.py
@@ -0,0 +1,53 @@
+# 5. 最长回文子串
+# 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+#
+# 示例 1：
+#
+# 输入: "babad"
+# 输出: "bab"
+# 注意: "aba" 也是一个有效答案。
+# 示例 2：
+#
+# 输入: "cbbd"
+# 输出: "bb"
+#
+# 来源：力扣（LeetCode）
+# 链接：https://leetcode-cn.com/problems/longest-palindromic-substring
+# 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+# 题解
+# https://leetcode-cn.com/problems/longest-palindromic-substring/solution/zhong-xin-kuo-san-dong-tai-gui-hua-by-liweiwei1419/
+
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        # 动态规划
+        size = len(s)
+        if size < 2:
+            return s
+
+        dp = [[False for _ in range(size)] for _ in range(size)]
+
+        max_len = 1
+        start = 0
+
+        for i in range(size):
+            dp[i][i] = True
+
+        for j in range(1, len(s)):
+            for i in range(j):
+                if s[i] == s[j]:
+                    if j - i < 3:
+                        dp[i][j] = True
+                    else:
+                        dp[i][j] = dp[i + 1][j - 1]
+                else:
+                    dp[i][j] = False
+
+                if dp[i][j]:
+                    cur_len = j - i + 1
+                    if cur_len > max_len:
+                        max_len = cur_len
+                        start = i
+
+        return s[start:start + max_len]
