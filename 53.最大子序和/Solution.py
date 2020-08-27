82a1eefcecd7953a43b480c38c97661eed2739f5
wangminglei <wangminglei@inke.cn>
2020-08-27 10:19:31 +0800

    53. 最大子序和 动态规划 AC

---
+++ 53.最大子序和/Solution.py
@@ -0,0 +1,33 @@
+"""
+53. 最大子序和
+给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+
+示例:
+
+输入: [-2,1,-3,4,-1,2,1,-5,4]
+输出: 6
+解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
+进阶:
+
+如果你已经实现复杂度为 O(n) 的解法，尝试使用更为精妙的分治法求解。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/maximum-subarray
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+from typing import List
+
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        # 动态规划
+        if not len(nums):
+            return 0
+
+        max_number = nums[0]
+        pre_max = 0
+        for num in nums:
+            pre_max = max(num, pre_max + num)
+            max_number = max(pre_max, max_number)
+
+        return max_number
