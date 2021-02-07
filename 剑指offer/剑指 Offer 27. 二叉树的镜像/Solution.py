e3a01b7531acf247fe29c890b6fcfb2c51ec6bbf
wangminglei <wangminglei@inke.cn>
2021-02-07 17:28:13 +0800

    剑指 Offer 27. 二叉树的镜像

---
+++ 剑指offer/剑指 Offer 27. 二叉树的镜像/Solution.py
@@ -0,0 +1,51 @@
+# 剑指 Offer 27. 二叉树的镜像
+# 请完成一个函数，输入一个二叉树，该函数输出它的镜像。
+#
+# 例如输入：
+#
+#      4
+#    /   \
+#   2     7
+#  / \   / \
+# 1   3 6   9
+# 镜像输出：
+#
+#      4
+#    /   \
+#   7     2
+#  / \   / \
+# 9   6 3   1
+#
+# 示例 1：
+# 输入：root = [4,2,7,1,3,6,9]
+# 输出：[4,7,2,9,6,3,1]
+#
+# 限制：
+# 0 <= 节点个数 <= 1000
+#
+# 注意：本题与主站 226 题相同：https://leetcode-cn.com/problems/invert-binary-tree/
+#
+# 来源：力扣（LeetCode）
+# 链接：https://leetcode-cn.com/problems/er-cha-shu-de-jing-xiang-lcof
+# 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+# 递归
+class Solution:
+    def mirrorTree(self, root: TreeNode) -> TreeNode:
+        def recursive(node: TreeNode) -> TreeNode:
+            # 终止条件
+            if not node:
+                return None
+            tmp = recursive(node.right)
+            node.right = recursive(node.left)
+            node.left = tmp
+            return node
+
+        return recursive(root)
