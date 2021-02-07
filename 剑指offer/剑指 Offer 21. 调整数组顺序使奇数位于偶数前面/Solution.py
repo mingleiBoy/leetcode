f96be56cdce78fcc80cb375e372455d25e06a78a
wangminglei <wangminglei@inke.cn>
2021-02-07 11:23:00 +0800

    剑指 Offer 21. 调整数组顺序使奇数位于偶数前面

---
+++ 剑指offer/剑指 Offer 21. 调整数组顺序使奇数位于偶数前面/Solution.py
@@ -0,0 +1,28 @@
+# 剑指 Offer 21. 调整数组顺序使奇数位于偶数前面
+# 输入一个整数数组，实现一个函数来调整该数组中数字的顺序，使得所有奇数位于数组的前半部分，所有偶数位于数组的后半部分。
+#  
+# 示例：
+# 输入：nums = [1,2,3,4]
+# 输出：[1,3,2,4]
+# 注：[3,1,2,4] 也是正确的答案之一。
+#
+# 来源：力扣（LeetCode）
+# 链接：https://leetcode-cn.com/problems/diao-zheng-shu-zu-shun-xu-shi-qi-shu-wei-yu-ou-shu-qian-mian-lcof
+# 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+from typing import List
+
+
+# 双指针试试
+# 独立AC，双指针有点意思
+class Solution:
+    def exchange(self, nums: List[int]) -> List[int]:
+        i, j = 0, len(nums)-1
+        while i < j:
+            if nums[i] % 2 != 0:
+                i += 1
+            elif nums[j] % 2 == 0:
+                j -= 1
+            else:
+                nums[i], nums[j] = nums[j], nums[i]
+
+        return nums
